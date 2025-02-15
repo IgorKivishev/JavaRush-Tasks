4217df7d6369f79ede2ff85481c6f6aa9e1942eb
Igor Kivishev <dxarmus@gmail.com>
2025-02-15 22:07:15 +0300

    Delete upload_to_git.bat

--- upload_to_git.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-chcp 65001
-cd /d C:\Users\dxarm\javarush\3537657\javarush-project
-git pull origin main --allow-unrelated-histories
-git add .
-git commit -m "Решение задачи: %date%"
-git push origin main
-pause
