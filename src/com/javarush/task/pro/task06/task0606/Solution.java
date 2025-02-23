bf76dcd9fb744a6832e1783c616b2ba45c2476e0
Igor K <dxarmus@gmail.com>
2025-02-23 22:41:49 +0300

    Task solution: 23.02.2025

---
+++ src/com/javarush/task/pro/task06/task0606/Solution.java
@@ -0,0 +1,45 @@
+package com.javarush.task.pro.task06.task0606;
+
+/* 
+Универсальный солдат
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static void universalMethod(){
+
+    }
+
+    public static void universalMethod(int a){
+
+    }
+    public static void universalMethod(int a, String b){
+
+    }
+    public static void universalMethod(int a, double b){
+
+    }
+    public static void universalMethod(int a, float b){
+
+    }
+    public static void universalMethod(int a, long b){
+
+    }
+    public static void universalMethod(int a, byte b){
+
+    }
+    public static void universalMethod(int a, short b){
+
+    }
+    public static void universalMethod(int a, char b){
+
+    }
+    public static void universalMethod(int a, int[] b){
+
+    }
+
+}
