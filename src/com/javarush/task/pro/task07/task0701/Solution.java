23652131f400a32ee4078ad85d98da03c485ba79
Igor K <dxarmus@gmail.com>
2025-02-27 22:58:50 +0300

    Task solution: 27.02.2025

---
+++ src/com/javarush/task/pro/task07/task0701/Solution.java
@@ -0,0 +1,17 @@
+package com.javarush.task.pro.task07.task0701;
+
+/* 
+Все возможные типы
+*/
+
+public class Solution {
+    byte a = 127;
+    short b = 32000;
+    int c = 1000000;
+    long d = 4221;
+    float e = 3.14f;
+    double f = 4.221d;
+    boolean g = true;
+    char h = 2;
+
+}
