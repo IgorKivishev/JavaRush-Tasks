70738d6e711379a2c78da9d53f570014ef3421e2
Igor K <dxarmus@gmail.com>
2025-03-25 23:23:42 +0300

    Task solution: 25.03.2025

---
+++ src/com/javarush/task/pro/task10/task1017/Earth.java
@@ -0,0 +1,17 @@
+package com.javarush.task.pro.task10.task1017;
+
+/* 
+Создание материков
+*/
+
+public class Earth {
+    public static void main(String[] args) {
+        Africa africa = new Africa(10000);
+        Antarctica antarctica = new Antarctica(11000);
+        Australia australia = new Australia(12000);
+        Eurasia eurasia = new Eurasia(13000);
+        NorthAmerica northAmerica = new NorthAmerica(14000);
+        SouthAmerica southAmerica = new SouthAmerica(15000);
+
+    }
+}
---
+++ src/com/javarush/task/pro/task10/task1020/Computer.java
@@ -0,0 +1,9 @@
+package com.javarush.task.pro.task10.task1020;
+
+public class Computer {
+    Mouse mouse = new Mouse();
+    Monitor monitor = new Monitor();
+    SystemUnit systemUnit = new SystemUnit();
+    Keyboard keyboard = new Keyboard();
+
+}
